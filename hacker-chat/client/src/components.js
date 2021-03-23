1f7a743e4c770cf60e6d83da85267b6fc1e98cbf
Leonardo Sousa <leonardolsousa5@gmail.com>
2021-03-23 09:12:38 -0300

    Configurando components

---
+++ hacker-chat/client/src/components.js
@@ -0,0 +1,40 @@
+import blessed from 'blessed'
+
+export default class ComponentsBuilder {
+    #screen
+    #layout
+
+    constructor() { }
+
+    #baseComponent() {
+        return {
+            border: 'line',
+            mouse: true,
+            keys: true,
+            top: 0,
+            scrollboar: {
+                ch: ' ',
+                inverse: true
+            },
+            //habilita colocar cores e tags no texto
+            tags: true
+        }
+    }
+
+    setScreen({ title }) {
+        this.#screen = blessed.screen({
+            smartCSR: true,
+            title
+        })
+        return this
+    }
+
+    setLayoutComponent() {
+        this.#layout = blessed.layout({
+            parent: this.#screen,
+            width: '100%',
+            height: '100%',
+        })
+        return this
+    }
+}
