2efd2f959717860cfb7b39fa923a44e89801d10e
Leonardo Sousa <leonardolsousa5@gmail.com>
2021-03-23 09:28:29 -0300

    Configurando components e controllers

--- hacker-chat/client/src/components.js
+++ hacker-chat/client/src/components.js
@@ -3,6 +3,7 @@ import blessed from 'blessed'
 export default class ComponentsBuilder {
     #screen
     #layout
+    #input
 
     constructor() { }
 
@@ -26,6 +27,8 @@ export default class ComponentsBuilder {
             smartCSR: true,
             title
         })
+        this.#screen.key(['escape', 'q', 'C-c'], () => process.exit(0))
+
         return this
     }
 
@@ -36,5 +39,37 @@ export default class ComponentsBuilder {
             height: '100%',
         })
         return this
+    }
+
+    setInputComponent(onEnterPressed) {
+        const input = blessed.textarea({
+            parent: this.#screen,
+            bottom: 0,
+            height: '10%',
+            inputOnFocus: true,
+            padding: {
+                top: 1,
+                left: 2
+            },
+            style: {
+                fg: '#f6f6f6',
+                bg: '#353535'
+            }
+        })
+
+        input.key('enter', onEnterPressed)
+        this.#input = input
+
+        return this
+
+    }
+
+    build() {
+        const components = {
+            screen: this.#screen,
+            input: this.#input
+        }
+
+        return components
     }
 }
