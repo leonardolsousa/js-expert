99e2718432da88bd4a6dd4ac680e0aadd7649ecf
Leonardo Sousa <leonardolsousa5@gmail.com>
2021-03-24 11:56:50 -0300

    Socket server rodando na porta definida

--- hacker-chat/server/src/socket.js
+++ hacker-chat/server/src/socket.js
@@ -12,7 +12,7 @@ export default class SocketServer {
         })
 
         return new Promise((resolve, reject) => {
-            server.on('error')
+            server.on('error', reject)
             server.listen(this.port, () => resolve(server))
         })
     }
