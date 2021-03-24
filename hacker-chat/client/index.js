1b12cf9bfd440db8966d4b32d335fff02163f91e
Leonardo Sousa <leonardolsousa5@gmail.com>
2021-03-23 21:27:08 -0300

    Criação do cliconfig

--- hacker-chat/client/index.js
+++ hacker-chat/client/index.js
@@ -1,5 +1,18 @@
+/*
+node index.js
+    --username leonardo \
+    --room sala01 \
+    --hostUri localhost
+ */
+
+
 import Events from 'events'
+import CliConfig from './src/cliConfig.js';
 import TerminalController from "./src/terminalController.js";
+
+const [nodePath, filePath, ...commands] = process.argv
+const config = CliConfig.parseArguments(commands)
+console.log('config', config)
 
 const componentEmitter = new Events()
 
